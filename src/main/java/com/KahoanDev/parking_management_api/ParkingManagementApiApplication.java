261153008ad3aaa12ab786393a308dfa979924f3
KahoanDev <kahoan9@gmail.com>
2025-01-01 20:50:33 -0300

    :tada: First Commit

---
+++ src/main/java/com/KahoanDev/parking_management_api/ParkingManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.KahoanDev.parking_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KahoanDev/parking_management_api/ParkingManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KahoanDev.parking_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
